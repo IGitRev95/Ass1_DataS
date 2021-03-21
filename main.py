cd72ba6614f541429d4ba9813de4898bbb0fe490
Ido <idorev@post.bgu.ac.il>
2021-03-21 22:17:01 +0200

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    print("hi")
+
+
+def ex1p1():
+    mat_a = np.asmatrix([[2, 1, 2],
+                         [1, -2, 1],
+                         [1, 2, 3],
+                         [1, 1, 1]])
+
+
+if __name__ == '__main__':
+    main()
