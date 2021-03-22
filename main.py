ed2737d42eb74e4f8d5ea40f54191b1698c8b75f
Ido <idorev@post.bgu.ac.il>
2021-03-22 12:13:13 +0200

    ex1p1 - done

--- main.py
+++ main.py
@@ -11,9 +11,21 @@ def ex1p1():
                          [1, -2, 1],
                          [1, 2, 3],
                          [1, 1, 1]])
-    vec_b = np.asarray([[6], [1], [5], [2]])
-    # x = np.tra
-    print(mat_a)
+    vec_b = np.asarray([[6],
+                        [1],
+                        [5],
+                        [2]])
+    least_squares(mat_a, vec_b)
+
+
+def least_squares(matrix_a: np.matrix, vector_b: np.asarray):
+    try:
+        ans_vector = np.linalg.inv(matrix_a.T @ matrix_a) @ matrix_a.T @ vector_b
+    except:
+        print("(A.T @ A) is a Singular matrix therefore not invertible")
+
+    print("x = \n {}".format(ans_vector))
+    print("r = Ax - b = \n {}".format(((matrix_a @ ans_vector) - vector_b)))
 
 
 if __name__ == '__main__':
