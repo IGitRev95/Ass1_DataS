bbc98fa1cd739431e6689adbdabb8753ad6c905e
Ido <idorev@post.bgu.ac.il>
2021-03-22 13:19:46 +0200

    ex1 - a & c - complete

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    ex1p1()
+    ex1()
 
 
-def ex1p1():
+def ex1():
     mat_a = np.asmatrix([[2, 1, 2],
                          [1, -2, 1],
                          [1, 2, 3],
@@ -15,7 +15,13 @@ def ex1p1():
                         [1],
                         [5],
                         [2]])
-    least_squares(mat_a, vec_b)
+    ex1a(mat_a, vec_b)
+    ex1c(mat_a, vec_b)
+
+
+def ex1a(matrix_a: np.matrix, vector_b: np.asarray):
+    print("regular least squares - ex1.a")
+    least_squares(matrix_a, vector_b)
 
 
 def least_squares(matrix_a: np.matrix, vector_b: np.asarray):
@@ -23,6 +29,23 @@ def least_squares(matrix_a: np.matrix, vector_b: np.asarray):
         ans_vector = np.linalg.inv(matrix_a.T @ matrix_a) @ matrix_a.T @ vector_b
     except:
         print("(A.T @ A) is a Singular matrix therefore not invertible")
+
+    print("x = \n {}".format(ans_vector))
+    print("r = Ax - b = \n {}".format(((matrix_a @ ans_vector) - vector_b)))
+
+
+def ex1c(matrix_a: np.matrix, vector_b: np.asarray):
+    print("weighted least squares - ex1.c")
+    weights_matrix = np.eye(4)
+    weights_matrix[0][0] = 100
+    weighted_least_squares(matrix_a, vector_b, weights_matrix)
+
+
+def weighted_least_squares(matrix_a: np.matrix, vector_b: np.asarray, weights_matrix: np.matrix):
+    try:
+        ans_vector = np.linalg.inv(matrix_a.T @ weights_matrix @ matrix_a) @ matrix_a.T @ weights_matrix @ vector_b
+    except:
+        print("(A.T @ W @ A) is a Singular matrix therefore not invertible")
 
     print("x = \n {}".format(ans_vector))
     print("r = Ax - b = \n {}".format(((matrix_a @ ans_vector) - vector_b)))
