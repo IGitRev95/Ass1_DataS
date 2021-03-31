8b8047343221f76b7569c06376f97771d5d49828
Ido <idorev@post.bgu.ac.il>
2021-03-31 14:25:27 +0300

    ex1 - fixed

--- main.py
+++ main.py
@@ -28,49 +28,51 @@ def ex1():
 
 
 def ex1a(matrix_a: np.matrix, vector_b: np.asarray):
-    print("regular least squares - ex1.a")
+    print("regular least squares - Results - ex1.a")
     least_squares(matrix_a, vector_b)
 
 
 def least_squares(matrix_a: np.matrix, vector_b: np.asarray):
-    try:
-        ans_vector = np.linalg.inv(matrix_a.T @ matrix_a) @ matrix_a.T @ vector_b
-    except:
-        print("(A.T @ A) is a Singular matrix therefore not invertible")
 
+    ans_vector = np.linalg.inv(matrix_a.T @ matrix_a) @ matrix_a.T @ vector_b
     print("x = \n {}".format(ans_vector))
     print("r = Ax - b = \n {}".format(((matrix_a @ ans_vector) - vector_b)))
 
 
 def ex1c(matrix_a: np.matrix, vector_b: np.asarray):
-    print("weighted least squares - ex1.c")
+
+    print("weighted least squares - Results - ex1.c")
     weights_matrix = np.eye(4)
-    weights_matrix[0][0] = 100
-    weighted_least_squares(matrix_a, vector_b, weights_matrix)
+    weight_0 = 1
+    r_0 = 1
+    r = 0
+    while np.abs(r_0) > (10 ** (-3)):
+        weights_matrix[0][0] = weight_0
+        result_weighted_ls = weighted_least_squares(matrix_a, vector_b, weights_matrix)
+        r = matrix_a @ result_weighted_ls - vector_b
+        r_0 = r[0][0]
+        weight_0 = weight_0 + 1
+
+    print("x = \n{} \nr = Ax - b = \n{} \nr_0 = {} \nweight_0 = {}".format(result_weighted_ls, r, r_0, (weight_0-1)))
 
 
 def weighted_least_squares(matrix_a: np.matrix, vector_b: np.asarray, weights_matrix: np.matrix):
-    try:
-        ans_vector = np.linalg.inv(matrix_a.T @ weights_matrix @ matrix_a) @ matrix_a.T @ weights_matrix @ vector_b
-    except:
-        print("(A.T @ W @ A) is a Singular matrix therefore not invertible")
-
-    print("x = \n {}".format(ans_vector))
-    print("r = Ax - b = \n {}".format(((matrix_a @ ans_vector) - vector_b)))
+    return np.linalg.inv(matrix_a.T @ weights_matrix @ matrix_a) @ matrix_a.T @ weights_matrix @ vector_b
 
 
 def ex1d(matrix_a: np.matrix, vector_b: np.asarray):
-    print("Tikhonov regularization least squares - ex1.d")
+
+    print("Tikhonov regularization least squares - Results - ex1.d")
     tikhonov_least_squares(matrix_a, vector_b, 0.1)
 
 
 def tikhonov_least_squares(matrix_a: np.matrix, vector_b: np.asarray, lambda_val: float):
+
     matrix_lambda = np.eye(3) * lambda_val
     ans_vector = np.linalg.inv((matrix_a.T @ matrix_a) + matrix_lambda) @ matrix_a.T @ vector_b
-
-    print("x = \n {}".format(ans_vector))
-    print("r = Ax - b = \n {}".format(((matrix_a @ ans_vector) - vector_b)))
+    print("x = \n{}\nr = Ax - b =\n{}".format(ans_vector, ((matrix_a @ ans_vector) - vector_b)))
 
 
 if __name__ == '__main__':
     main()
+
